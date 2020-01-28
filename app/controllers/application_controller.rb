47f39830204ca372a9e0ad34a897f4139af9a510
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-01-28 15:42:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < Sinatra::Base
   erb :edit
   end
   
-  patch '/articles/:id' do
+   patch '/articles/:id' do
     @article = Article.find(params[:id])
     @article.title = params[:title]
     @article.content = params[:content]
